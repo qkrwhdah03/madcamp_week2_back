6c30c15bc08b70b6c90449e4aa912c7f7f190ee5
qkrwhdah03 <qkrwhdah03@naver.com>
2024-01-06 00:17:25 +0900

    divide trainer & member app

--- app/src/main/java/com/example/week2/MainActivity.java
+++ app/src/main/java/com/example/week2/MainActivity.java
@@ -18,22 +18,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // Kakao 로그인 후, 가입 정보 없다 -> 가입
         //                가입 정보 있다 -> 회원, 트레이너 정보에 따라 Activity 이덩
 
         //(1) Kakao 로그인 후 정보 얻어오기 + 서버에 존재성 여부 요청
-
         if(!IsMember){ // 멤버가 아니라면 프로필 생성 처리 후 이동
 
         }
         if(IsTrainer){
             Intent trainer_intent = new Intent(this, TrainerActivity.class);
-            //startActivity(trainer_intent);
+            startActivity(trainer_intent);
+            finish();
         }
         else{
             Intent user_intent = new Intent(this, MemberActivity.class);
             startActivity(user_intent);
+            finish();
         }
     }
 }
