f7c50441dfe0385007db2d04a52faba3ee05e176
qkrwhdah03 <qkrwhdah03@naver.com>
2024-01-06 12:01:31 +0900

    basic structure

---
+++ app/src/main/java/com/example/week2/ApplicationActivity.java
@@ -0,0 +1,25 @@
+package com.example.week2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ApplicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application);
+
+
+
+        // Main Activity로 값 전달.. 제출버튼 클릭시
+        /*
+        Intent intent = new Intent(ApplicationActivity.this, MainActivity.class);
+        intent.putExtra("result","resultData");
+        setResult(RESULT_OK, intent);
+        finish();
+         */
+    }
+}
--- app/src/main/java/com/example/week2/MainActivity.java
+++ app/src/main/java/com/example/week2/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.week2;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean IsMember;
     private boolean IsTrainer;
 
+    ActivityResultLauncher<Intent> StartForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode() == RESULT_OK){
+                    Intent intent = result.getData();
+                    Log.d("MainActivity", intent.getStringExtra("result"));
+                }
+            }
+    );
+
     public MainActivity(){
+        /*
+        IsMember IsTrainer
+         true     true    --> 바로 trainer 앱으로 넘어감
+         true     false   --> 바로 member 앱으로 넘어감
+         false            --> 입력 폼 창으로 넘어감
+         */
         IsMember = true;
         IsTrainer = true;
     }
@@ -23,17 +42,19 @@ public class MainActivity extends AppCompatActivity {
 
         //(1) Kakao 로그인 후 정보 얻어오기 + 서버에 존재성 여부 요청
         if(!IsMember){ // 멤버가 아니라면 프로필 생성 처리 후 이동
-
+            Intent application_intent = new Intent(this, ApplicationActivity.class);
+            StartForResult.launch(application_intent);
         }
-        if(IsTrainer){
-            Intent trainer_intent = new Intent(this, TrainerActivity.class);
-            startActivity(trainer_intent);
-            finish();
-        }
-        else{
-            Intent user_intent = new Intent(this, MemberActivity.class);
-            startActivity(user_intent);
-            finish();
+        else {
+            if (IsTrainer) {
+                Intent trainer_intent = new Intent(this, TrainerActivity.class);
+                startActivity(trainer_intent);
+                finish();
+            } else {
+                Intent user_intent = new Intent(this, MemberActivity.class);
+                startActivity(user_intent);
+                finish();
+            }
         }
     }
 }
