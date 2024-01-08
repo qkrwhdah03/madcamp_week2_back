653c2c9eeb24f71c60e0b355aa1165d8cc621ea7
dahee <daheekang00@gmail.com>
2024-01-08 21:33:45 +0900

    woongweek2

--- app/src/main/java/com/example/week2/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/week2/ui/home/HomeFragment.java
@@ -9,17 +9,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
-import com.example.week2.ProfileAdapter;
-import com.example.week2.R;
 import com.example.week2.databinding.FragmentHomeBinding;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
 
@@ -32,36 +23,6 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
-
-        View recyclerView = root.findViewById(R.id.recyclerView); // RecyclerView 초기화
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
-
-        // Retrofit을 사용하여 프로필 데이터를 가져옴
-        ProfileApi profileApi = RetrofitClient.getInstance().create(ProfileApi.class);
-        Call<List<Profile>> call = profileApi.getProfiles();
-        call.enqueue(new Callback<List<Profile>>() {
-            @Override
-            public void onResponse(Call<List<Profile>> call, Response<List<Profile>> response) {
-                if (response.isSuccessful()) {
-                    List<Profile> profiles = response.body();
-                    profileAdapter = new ProfileAdapter(profiles);
-                    recyclerView.setAdapter(profileAdapter);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Profile>> call, Throwable t) {
-                // 오류 처리
-            }
-        });
-
-
-
 
         return root;
     }
